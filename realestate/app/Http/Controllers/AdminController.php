4949ecc85db609bfb58a39a93c029d2ef035b88d
Ahmadmarei2001 <81181915+Ahmadmarei2001@users.noreply.github.com>
2023-09-10 11:33:30 +0300

    First Commit

---
+++ realestate/app/Http/Controllers/AdminController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+class AdminController extends Controller
+{
+    public function adminDashboard() { 
+        return view('admin.index');
+    }
+
+    public function adminLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }
+
+    public function adminLogin(){
+        return view('admin.admin_login');
+    }
+
+    public function adminProfile(){
+        $id = Auth::user()->id;
+        $profilData = User::find($id);
+        return view('admin.admin_profile_view',compact('profilData'));
+    }
+    public function adminProfileStore(Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            $filename = date('YmdHi').$file->getClientOriginalName(); 
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo'] = $filename;
+            
+
+           
+
+            
+
+        }
+        $data->save();
+        return redirect()->back();
+
+    }
+}
